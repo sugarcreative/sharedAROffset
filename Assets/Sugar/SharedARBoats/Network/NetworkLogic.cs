ac0ff1ae6efab3f9a3236e39608644945b5c9bd6
KiranAtSugar <kiran@sugarcs.com>
2024-08-09 17:38:07 +0100

    Added better networking logic and flow for joining as client

--- Assets/Sugar/SharedARBoats/Network/NetworkLogic.cs
+++ Assets/Sugar/SharedARBoats/Network/NetworkLogic.cs
@@ -1,7 +1,5 @@
 using Niantic.Lightship.AR.LocationAR;
 using Niantic.Lightship.SharedAR.Colocalization;
-using System;
-using System.Runtime.InteropServices;
 using TMPro;
 using Unity.Netcode;
 using UnityEngine;
@@ -12,6 +10,8 @@ public class NetworkLogic : MonoBehaviour
     [SerializeField]
     private TMP_Text _statusText, _roomNameDisplayText;
 
+    [SerializeField] private TMP_InputField displayNameInputField, roomCodeInputField;
+
     [SerializeField]
     private Button _joinAsHostButton;
 
@@ -19,10 +19,15 @@ public class NetworkLogic : MonoBehaviour
     private Button _joinAsClientButton;
 
     [SerializeField]
+    private Button _roomCodeEnterButton;
+
+    [SerializeField]
     private SharedSpaceManager _sharedSpaceManager;
 
     [SerializeField]
     private ARLocation arLocation;
+
+    [SerializeField] private GameObject hostJoinPanel, enterRoomCodePanel;
 
     private string _roomName;
 
@@ -32,13 +37,20 @@ public class NetworkLogic : MonoBehaviour
     {
         DontDestroyOnLoad(gameObject);
 
-        _roomName = "Hello";
-        _joinAsClientButton.gameObject.SetActive(false);
-        _joinAsHostButton.gameObject.SetActive(false);
+        //_roomName = "Hello"; // have room name become random number generation, then host only has to enter name.
+        hostJoinPanel.SetActive(false);
+        enterRoomCodePanel.SetActive(false);
+        //roomCodeInputField.gameObject.SetActive(false);
+        //_joinAsClientButton.gameObject.SetActive(false);
+        //_joinAsHostButton.gameObject.SetActive(false);
 
+        roomCodeInputField.onValueChanged.AddListener(OnRoomCodeChanged);
+        displayNameInputField.onValueChanged.AddListener(OnDiplayNameChanged);
 
         _joinAsHostButton.onClick.AddListener(OnJoinAsHostClicked);
         _joinAsClientButton.onClick.AddListener(OnJoinAsClientClicked);
+        _joinAsClientButton.interactable = false;
+        _joinAsHostButton.interactable = false;
 
 
         NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnectedCallback;
@@ -48,40 +60,111 @@ public class NetworkLogic : MonoBehaviour
         _sharedSpaceManager.sharedSpaceManagerStateChanged += OnColocalizationTrackingStateChanged;
     }
 
+
+
     private void OnColocalizationTrackingStateChanged(
         SharedSpaceManager.SharedSpaceManagerStateChangeEventArgs args)
     {
         if (args.Tracking)
         {
             if (_isJoined) return;
-            _joinAsHostButton.gameObject.SetActive(true);
-            _joinAsClientButton.gameObject.SetActive(true);
+            //_joinAsHostButton.gameObject.SetActive(true);
+            //_joinAsClientButton.gameObject.SetActive(true);
+            hostJoinPanel.SetActive(true);
         }
     }
 
     private void OnJoinAsHostClicked()
     {
+        if (displayNameInputField.text.Equals("")) return;
+        //Maybe have some popup that tells them to input a name.
+        _roomName = Random.Range(0, 9999).ToString();
+
         var topts = ISharedSpaceTrackingOptions.CreateVpsTrackingOptions(arLocation);
         var ropts = SetUpRoomAndUI(topts);
         StartSharedSpace(topts, ropts);
         NetworkManager.Singleton.StartHost();
-        _joinAsHostButton.gameObject.SetActive(false);
-        _joinAsClientButton.gameObject.SetActive(false);
+        //_joinAsHostButton.gameObject.SetActive(false);
+        //_joinAsClientButton.gameObject.SetActive(false);
+        hostJoinPanel.SetActive(false);
         _isJoined = true;
-
     }
 
     private void OnJoinAsClientClicked()
     {
-        
+
+        EnterRoomCode();
+        //Maybe have some popup that tells them to input a name.
+
+        //Press the join button, a window pops up and prompts them for a room code, once entered then continue
+
+
+
+        //var topts = ISharedSpaceTrackingOptions.CreateVpsTrackingOptions(arLocation);
+        //var ropts = SetUpRoomAndUI(topts);
+        //StartSharedSpace(topts, ropts);
+        //NetworkManager.Singleton.StartClient();
+
+
+        // DO SOME CHECK HERE TO SEE IF A LOBBY WITH THE PIN EXISTS
+        //_joinAsHostButton.gameObject.SetActive(false);
+        //_joinAsClientButton.gameObject.SetActive(false);
+
+
+        //hostJoinPanel.SetActive(false);
+        //_isJoined = true;
+    }
+
+    private void EnterRoomCode()
+    {
+        enterRoomCodePanel.SetActive(true);
+    }
+
+    private void OnRoomCodeChanged(string text)
+    {
+        if(!string.IsNullOrEmpty(text))
+        {
+            _roomCodeEnterButton.interactable = true;
+        }
+        else
+        {
+            _roomCodeEnterButton.interactable = false;
+        }
+    }
+
+    private void OnDiplayNameChanged(string displayName)
+    {
+        if (!string.IsNullOrEmpty(displayName))
+        {
+            _joinAsHostButton.interactable = true;
+            _joinAsClientButton.interactable = true;
+        }
+        else
+        {
+            _joinAsHostButton.interactable = false;
+            _joinAsClientButton.interactable = false;
+        }
+    }
+
+    public void OnConfirmButtonClicked()
+    {
+        _roomName = roomCodeInputField.text;
+        enterRoomCodePanel.SetActive(false);
+
         var topts = ISharedSpaceTrackingOptions.CreateVpsTrackingOptions(arLocation);
         var ropts = SetUpRoomAndUI(topts);
         StartSharedSpace(topts, ropts);
         NetworkManager.Singleton.StartClient();
-        _joinAsHostButton.gameObject.SetActive(false);
-        _joinAsClientButton.gameObject.SetActive(false);
+
+
+        // DO SOME CHECK HERE TO SEE IF A LOBBY WITH THE PIN EXISTS
+        //_joinAsHostButton.gameObject.SetActive(false);
+        //_joinAsClientButton.gameObject.SetActive(false);
+
+
+        hostJoinPanel.SetActive(false);
         _isJoined = true;
-    }
+    } 
 
     private void HideButtons()
     {
@@ -89,16 +172,19 @@ public class NetworkLogic : MonoBehaviour
         _joinAsClientButton.gameObject.SetActive(false);
     }
 
+    private void HideHostJoinPanel()
+    {
+        hostJoinPanel.SetActive(false);
+    }
+
     private void OnClientConnectedCallback(ulong clientId)
     {
         _statusText.text = $"{clientId} Connected";
-        //GameUIManager.PlayerJoin(Convert.ToInt32(clientId.ToString()));
     }
 
     private void OnClientDisconnectedCallback(ulong clientId)
     {
         _statusText.text = $"{clientId} Disconnected";
-        //GameUIManager.PlayerLeave(Convert.ToInt32(clientId.ToString()));
     }
 
     private ISharedSpaceRoomOptions SetUpRoomAndUI(ISharedSpaceTrackingOptions topts)
@@ -107,7 +193,6 @@ public class NetworkLogic : MonoBehaviour
         _roomNameDisplayText.gameObject.SetActive(true);
 
         return ISharedSpaceRoomOptions.CreateVpsRoomOptions(topts, _roomName, 10, "vps_boats");
-
     }
 
     private void StartSharedSpace(ISharedSpaceTrackingOptions topts, ISharedSpaceRoomOptions ropts)

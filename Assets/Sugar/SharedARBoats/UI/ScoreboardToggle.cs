0d0a97190540387455bade801076a848f367314f
KiranAtSugar <kiran@sugarcs.com>
2024-08-12 18:31:15 +0100

    Adding some Final UI assets, Network shooting appears to fully work

--- Assets/Sugar/SharedARBoats/UI/ScoreboardToggle.cs
+++ Assets/Sugar/SharedARBoats/UI/ScoreboardToggle.cs
@@ -6,7 +6,12 @@ public class ScoreboardToggle : MonoBehaviour
     [SerializeField] GameObject scoreBoard;
     public void SetActive()
     {
-        bool boolToSet = scoreBoard.gameObject.activeInHierarchy ? false: true;
-        scoreBoard.SetActive(boolToSet);
+        if (scoreBoard.activeInHierarchy)
+        {
+            scoreBoard.SetActive(false);
+        } else
+        {
+            scoreBoard.SetActive(true);
+        }
     }
 }

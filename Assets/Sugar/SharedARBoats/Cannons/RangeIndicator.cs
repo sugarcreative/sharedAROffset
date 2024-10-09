f52b549d7a26b4e8efccce27e470ad4284c7b2b2
KiranAtSugar <kiran@sugarcs.com>
2024-10-09 14:19:08 +0100

    Increase size and add range indicator

---
+++ Assets/Sugar/SharedARBoats/Cannons/RangeIndicator.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class RangeIndicator : MonoBehaviour
+{
+
+    [SerializeField] private ModalFade _fade;
+
+    private int _numOfCollisions;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_numOfCollisions != 0) {
+            _fade.Show();
+        }
+        else {
+            _fade.Hide();
+        }
+        Debug.Log(_numOfCollisions);
+    }
+
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("NetworkPlayer")) {
+            
+            _numOfCollisions++;
+            Debug.Log($"OnTriggerEnter: {gameObject.name}");
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("NetworkPlayer")) {
+            
+            _numOfCollisions--;
+            Debug.Log($"OnTriggerExit: {gameObject.name}");
+        }
+    }
+}
